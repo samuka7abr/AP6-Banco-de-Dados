3138a4f0df239776684e001f582ed2aafee82291
Joao Genaro <JoaoGenaro@Joaos-Macbook-Pro.local>
2025-04-01 09:37:39 -0300

    Alterações feitas

--- Arquivos/estacionamento.sql
+++ Arquivos/estacionamento.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE professor (
     id_professor INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
@@ -15,19 +14,19 @@ CREATE TABLE tag_estacionamento (
 
 CREATE TABLE pagamento (
     id_pagamento INT PRIMARY KEY AUTO_INCREMENT,
-    id_professor INT NOT NULL,
+    id_tag INT NOT NULL,
     valor DECIMAL(10,2) NOT NULL,
     forma_pagamento ENUM('Pix', 'Boleto', 'Visa', 'Mastercard') NOT NULL,
     data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_professor) REFERENCES professor(id_professor)
+    FOREIGN KEY (id_tag) REFERENCES tag_estacionamento(id_tag)
 );
 
 CREATE TABLE problemas (
     id_problema INT PRIMARY KEY AUTO_INCREMENT,
-    id_professor INT NOT NULL,
+    id_tag INT NOT NULL,
     descricao TEXT,
     data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_professor) REFERENCES professor(id_professor)
+    FOREIGN KEY (id_tag) REFERENCES tag_estacionamento(id_tag)
 );
 
 CREATE TABLE reserva (
@@ -35,7 +34,6 @@ CREATE TABLE reserva (
     id_professor INT NOT NULL,
     FOREIGN KEY (id_professor) REFERENCES professor(id_professor)
 );
-
 
 INSERT INTO professor (id_professor, nome, departamento) VALUES
 (1, 'Ana Paula', 'Engenharia'),
@@ -45,23 +43,18 @@ INSERT INTO professor (id_professor, nome, departamento) VALUES
 (5, 'Beatriz Gonçalves', 'Psicologia'),
 (6, 'Lucas Almeida', 'Arquitetura');
 
-
 INSERT INTO reserva (id_professor) VALUES
 (3), -- Carlos Silva
 (6); -- Lucas Almeida
-
 
 INSERT INTO tag_estacionamento (id_tag, id_professor, tag, status) VALUES
 (1, 1, 'TAG123ABC', 'ativa'),     -- Ana Paula
 (2, 2, 'TAG987ZYX', 'ativa'),     -- João Pedro
 (3, 4, 'TAG654TRE', 'inativa');   -- Carla Menezes
 
-
-INSERT INTO pagamento (id_pagamento, id_professor, valor, forma_pagamento, data_pagamento) VALUES
+INSERT INTO pagamento (id_pagamento, id_tag, valor, forma_pagamento, data_pagamento) VALUES
 (1, 1, 100.00, 'Visa', '2024-03-10 12:00:00'),   -- Ana Paula
 (2, 2, 120.00, 'Visa', '2024-03-15 09:30:00');   -- João Pedro
 
-
-INSERT INTO problemas (id_problema, id_professor, descricao, data_registro) VALUES
-(1, 1, 'Falha na leitura da tag', '2024-03-16 08:45:00'),   -- Ana Paula
-(2, 2, 'Tag desprogramada', '2024-03-17 14:10:00');         -- João Pedro
+INSERT INTO problemas (id_problema, id_tag, descricao, data_registro) VALUES
+(1, 3, 'Tag não reconhecida', '2024-03-20 08:15:00');
